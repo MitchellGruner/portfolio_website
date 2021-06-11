7cb33bd96099c8951033ea3b9d78843e39fad486
Mitchell Gruner <mgruner@csus.edu>
2021-06-11 16:54:32 -0500

    fix resume issue?

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function Resume(){
-    return <iframe src="https://drive.google.com/file/d/1E9Ew4HJtSJmnYsS_asI9tMg9U1HBJHyz/preview" title="resume" width="100%" height="600"></iframe>;
+    return <iframe src="https://drive.google.com/file/d/1Kawc_c7rP7rP--lpEWwFHuMoLtNOIR1D/preview" width="100%" height="600px"></iframe>;
 }
