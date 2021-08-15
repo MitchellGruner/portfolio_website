c02c2e26aba0aebcd6adee9d133e35f3de16d2f2
Mitchell Gruner <mgruner@csus.edu>
2021-08-15 12:52:33 -0500

    update resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function Resume(){
-    return <iframe src="https://drive.google.com/file/d/1HofIMss5fyhySdZj9KNWAcVuYnsL3svQ/preview" title="resume" width="100%" height="600px"></iframe>;
+    return <iframe src="https://drive.google.com/file/d/1KR5l4sDVaT4nb3GlX6OF75V7wCkCgkGc/preview" title="resume" width="100%" height="600px"></iframe>;
 }
