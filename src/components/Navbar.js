6e9e0828bc3a0a0b1da8a117a54e28ebc9963aaa
Mitchell Gruner <mit.gruner@gmail.com>
2022-12-03 19:20:26 -0600

    remove Mitchell Gruner and i tag from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,9 +28,6 @@ function Navbar() {
         <div>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        MITCHELL GRUNER <i class="fas fa-code"></i>
-                    </Link>
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
